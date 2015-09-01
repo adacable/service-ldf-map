8bf447c673e0c6aaec50296cab686f889fab8cb9
drcable Walpole <drcable@tarbwf.com>
2015-09-01 02:11:17 +0100

    initial commit, framework
    building the base on which to build the rest, begining to spec

---
+++ js/events.js
@@ -0,0 +1,19 @@
+{
+    type: "FeatureCollection",
+    features: [{
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [1, 1]
+        },
+        properties: { 
+            eventName: "Example Event",//must exist, be under 20ch
+            eventTime: 2012-04-23T18:25:43.511Z, //must exist
+            eventImage: "http://www.google.com/images/srpr/logo11w.png",//can be blank
+            eventLink:  "eventbrite.com",//can be blank
+            eventOrganizer: "A.N.Other", //can be blank
+            eventTags: ["tag1","tag2"],//can be blank
+            
+            }
+    }]
+}
