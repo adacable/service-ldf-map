8bd49b26780c647bdc02f40aa2e3ea255a6eb20f
drcable Walpole <drcable@users.noreply.github.com>
2015-09-24 23:03:59 +0100

    Adding all the events

--- events.js
+++ events.js
@@ -5,18 +5,18 @@ events = {
         type: "Feature",
         geometry: {
             type: "Point",
-            coordinates: [ -0.1275,51.5072]
+            coordinates: [-0.129108,51.501764]
         },
         properties: { 
             "marker-color": "#00ff00",
-            eventName: "Example Event",//must exist, be under 20ch
+            eventName: "Opportunities in Government, Policy Lab",//must exist, be under 20ch
             eventTime: "2012-04-23T18:25:43.511Z", //must exist
-            eventImage: "http://www.google.com/images/srpr/logo11w.png",//can be blank
-            eventLink:  "eventbrite.com",//can be blank
-            eventOrganizer: "A.N.Other", //can be blank
-            eventTags: ["tag1","tag2"],//can be blank
-            eventAddress: "",//cannot be blank
-            eventDescription : "test description",
+            eventImage: "https://openpolicy.blog.gov.uk/wp-content/uploads/sites/35/2014/07/Policy-Lab-digitisation-3.jpg",//can be blank
+            eventLink:  "https://servicedesignldf.wordpress.com/2015/09/09/opportunities-in-government-policy-lab/",//can be blank
+            eventOrganizer: "Policy lab", //can be blank
+            eventTags: [],//can be blank
+            eventAddress: "Cabinet Office, 1 Horse Guards Road, Westminster",
+            eventDescription : "Policy Lab is a creative space which supports Government departments to use innovative tools and techniques – including service design – to make better policies and public services for the public. We bring together policymakers, external designers, front-line service providers and members of the public to come up with fresh perspectives and ideas around tricky long-term policy problems.",
             
             }
     },
@@ -24,18 +24,87 @@ events = {
         type: "Feature",
         geometry: {
             type: "Point",
-            coordinates: [ -0.1385,51.5162]
+            coordinates: [ -0.108278,51.508347]
         },
         properties: { 
             "marker-color": "#00ff00",
-            eventName: "Example Event",//must exist, be under 20ch
+            eventName: "Working with government",//must exist, be under 20ch
             eventTime: "2012-04-23T18:25:43.511Z", //must exist
-            eventImage: "http://www.google.com/images/srpr/logo11w.png",//can be blank
-            eventLink:  "eventbrite.com",//can be blank
-            eventOrganizer: "A.N.Other", //can be blank
-            eventTags: ["tag1","tag2"],//can be blank
-            eventAddress: "2 somewhere or other \nanother place\nyeah",//cannot be blank
-            eventDescription : " esr toast listicle Wes Anderson. Fap actually chillwave, synth pop-up pour-over Truffaut craft beer beard deep v single-origin coffee lo-fi before they sold out scenester four dollar toast. cription",
+            eventImage: "https://img.evbuc.com/https%3A%2F%2Fimg.evbuc.com%2Fhttps%253A%252F%252Fcdn.evbuc.com%252Fimages%252F15447833%252F22818968422%252F1%252Foriginal.jpg%3Frect%3D0%252C77%252C3264%252C1632%26s%3Db87e49e11ef7eda5d2ce101f094e95b1?w=1000&s=cce7fc715576991efaf4a104e7a99e05",//can be blank
+            eventLink:  "https://www.eventbrite.co.uk/e/working-with-government-tickets-18606456412",//can be blank
+            eventOrganizer: "Lior smith", //can be blank
+            eventAddress: " Oxo Tower Wharf, Barge House Street,London , Gt Lon SE1 9PH ",//cannot be blank
+            eventDescription : "A trio of talks about working with government.",
             }
-    }]
+    },
+{
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [-0.107772,51.507249]
+        },
+        properties: { 
+            "marker-color": "#00ff00",
+            eventName: "Service design drinks",//must exist, be under 20ch
+            eventTime: "2012-04-23T18:25:43.511Z", //must exist
+            eventImage: "https://cdn.evbuc.com/eventlogos/25681595/10760374571.png",//can be blank
+            eventLink:  "http://www.eventbrite.co.uk/e/service-design-drinks-for-london-design-festival-tickets-18343447746",
+            eventOrganizer: "", //can be blank
+            eventAddress: " The Thirsty Bear, 62 Stamford Street, Blackfriars, London, SE1 9LX",//cannot be blank
+            eventDescription : "Having a busy week at the London Design Festival? Come for a drink to relax and share what you saw. The Service Design Drink is a free gathering for people are are practicing or studying Service Design. In a pub, over a beer, have a good chat with someone who shares the same passion :)",
+            }
+    },
+{
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [ -0.107,51.508347]
+        },
+        properties: { 
+            "marker-color": "#00ff00",
+            eventName: "WHAT IS SERVICE DESIGN?",//must exist, be under 20ch
+            eventTime: "2012-04-23T18:25:43.511Z", //must exist
+            eventImage: "https://img.evbuc.com/https%3A%2F%2Fimg.evbuc.com%2Fhttps%253A%252F%252Fcdn.evbuc.com%252Fimages%252F15565090%252F22818968422%252F1%252Foriginal.jpg%3Frect%3D0%252C109%252C1330%252C665%26s%3D500c2911cbd4afed7cc25ec6fa103dd4?w=480&s=3806e05e0578b7f56135ce1c33a4b031",//can be blank
+            eventLink:  "https://www.eventbrite.co.uk/e/working-with-government-tickets-18606456412k",
+            eventOrganizer: "Lior smith", //can be blank
+            eventAddress: " Oxo Tower Wharf, Barge House Street,London , Gt Lon SE1 9PH ",//cannot be blank
+            eventDescription : "A trio of talks about working with government.",
+            }
+    },
+	{
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [ -0.071102,51.525158]
+        },
+        properties: { 
+            "marker-color": "#00ff00",
+            eventName: "How to make personas in the public sector?",//must exist, be under 20ch
+            eventTime: "2012-04-23T18:25:43.511Z", //must exist
+            eventImage: "https://i2.wp.com/photos1.meetupstatic.com/photos/event/2/1/4/d/global_437168525.jpeg",//can be blank
+            eventLink:  "http://www.meetup.com/People-before-pixels/events/225371981/",
+            eventOrganizer: "Lior smith", //can be blank
+            eventAddress: "Newspeak House",//cannot be blank
+            eventDescription : "How do we make research actionable? How can research spread knowledge quickly and effectively? How do we encourage people to apply research in a way that isn’t prescriptive but encourages creativity around a problem? How do we increase awareness and empathy of user needs?",
+            }
+    },
+
+	{
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [ -0.132840 ,51.509687]
+        },
+        properties: { 
+            "marker-color": "#00ff00",
+            eventName: "Inside Design, Uscreates",//must exist, be under 20ch
+            eventTime: "2012-04-23T18:25:43.511Z", //must exist
+            eventImage: "https://i1.wp.com/uscreates.com/wp-content/uploads/2015/04/OUR-WORK_Cover_LOC_2.jpg",//can be blank
+            eventLink:  "http://uscreates.com/",
+            eventOrganizer: "Uscreates", //can be blank
+            eventAddress: " HUB Westminster, 1st Floor New Zealand House 80, 80 Haymarket, London, SW1Y 4TE",//cannot be blank
+            eventDescription : "Inside Design is a 2 hour workshop giving you the opportunity to experience the value design can deliver for you and your organisation",
+            }
+    }
+]
 }
